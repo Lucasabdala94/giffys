517345a1b5258ec4c3c8db5c71e79a4edad37272
lucas <lucasabdala94@gmail.com>
2023-02-06 00:31:29 -0300

    custom hooks 1:28

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,21 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import getGifs from "../services/getGifs";
+import GifsContext from "../context/GifsContext"
 
-export default function useGifs({ keyword } = { keyword: 'randon' }) {
-
+export default function useGifs({ keyword } = { keyword:null}) {
+    const {gifs,setGifs}= useContext(GifsContext)
     const [loading, setLoading] = useState(false)
-    const [gifs, setGifs] = useState([]);
 
     useEffect(function () {
         setLoading(true)
         const keywordToUse = keyword || localStorage.getItem('lastkeyword') || 'randon'
-        getGifs({ keyword: keywordToUse })
+        getGifs( { keyword:keywordToUse})
             .then(gifs => {
                 setGifs(gifs)
                 setLoading(false)
                 localStorage.setItem('lastkeyword', keyword)
             })
-    }, [keyword])
+    }, [keyword,setGifs])
 
     return { loading, gifs }
 
